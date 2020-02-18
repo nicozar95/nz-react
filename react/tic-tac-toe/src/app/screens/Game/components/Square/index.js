894b4f9310afb112e005f8c5ba177da342f1afb9
nicozar95 <nzarewsky@gmail.com>
2020-02-17 22:42:46 -0300

    Set state

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './styles.module.scss';
 
-const Square = () => (
-  <button type="button" className={styles.square}>
-    {/* TODO */}
-  </button>
-);
+class Square extends Component {
+  state = { value: null }
+  render(){
+    return(
+    <button
+      type="button"
+      className={styles.square}
+      onClick={ ()=> this.setState({value: 'X'}) }
+    >
+      {this.state.value}
+    </button>
+    )
+  }
+};
 
 export default Square;
