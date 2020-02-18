68089c8437c4127b980deab80faa4b6c262f436f
nicozar95 <nzarewsky@gmail.com>
2020-02-17 22:50:33 -0300

    Fix linter

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -4,17 +4,22 @@ import styles from './styles.module.scss';
 
 class Square extends Component {
   state = { value: null }
-  render(){
-    return(
-    <button
-      type="button"
-      className={styles.square}
-      onClick={ ()=> this.setState({value: 'X'}) }
-    >
-      {this.state.value}
-    </button>
-    )
+
+  handleClick = () => {
+    this.setState({ value: 'X' });
   }
-};
+
+  render() {
+    return (
+      <button
+        type="button"
+        className={styles.square}
+        onClick={this.handleClick}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
 
 export default Square;
